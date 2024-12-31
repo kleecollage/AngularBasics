c1c8293481b15d5389ab71b61fc5ed475a72eb8d
Klee Collage <klee.collage@gmail.com>
2024-12-31 15:46:33 -0600

    feat(mongo): Connect And fetching data with MOngoDb

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
-import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
+import Post from './models/post.js';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+//** DB CONNECTION **//
+mongoose.connect("mongodb+srv://mean_user:rootAccess@cluster0.zeg8wsi.mongodb.net/mean_posts?authMechanism=SCRAM-SHA-1")
+ .then(() => {
+    console.log("Connected to MongoDB!");
+ })
+ .catch(() => {
+    console.log('Connection failed');
+ });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/* CORS CONFIG V1
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -17,39 +29,38 @@ app.use((req, res, next) => {
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
-})
+}) */
 
+//** CORS CONFIG V2 **//
+app.use(cors({
+  origin: '*', // allow all hosts
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}));
+
+//** POST [ host/api/post ] **//
 app.post('/api/posts', (req,res,next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  // console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
+//** GET [ host/api/post ] **//
 app.get('/api/posts', (req, res, ext) => {
-  const posts = [
-    {
-      id: 'kAfce13',
-      title: 'First server-side post',
-      content: 'This is comming from the server'
-    },
-    {
-      id: 'lMdnw57',
-      title: 'Second server-side post',
-      content: 'This is comming from the server!'
-    },
-    {
-      id: 'mQkfn01',
-      title: 'Third server-side post',
-      content: 'This is comming from the server!!! uwu'
-    },
-  ]
-
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts,
-  });
+  Post.find()
+    .then(documents => {
+      // console.log(documents);
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: documents,
+      });
+    });
 });
 
 export default app
